80a910c566b7f0e57adcf89cfff67c56c5383ac5
AKAPP611 <Aknassar11@gmail.com>
2025-08-18 15:25:44 +0400

    Update factory-select.js

--- public/src/views/factory-select.js
+++ public/src/views/factory-select.js
@@ -86,9 +86,9 @@ function renderCards(grid, factories, prod) {
     let iconMarkup = '';
 
     if (f.key === 'Pistachio') {
-  iconMarkup = '<img src="src/assets/icons/pistachio.png" class="ws-icon pistachio-icon" alt="" style="width: 32px; height: 32px; margin-right: 8px;">';
+  iconMarkup = '<img src="src/assets/icons/pistachio.png" class="ws-icon pistachio-icon" alt="" style="width: 40px; height: 40px; margin-right: 8px;">';
 } else if (f.key === 'Walnut') {
-  iconMarkup = '<img src="src/assets/icons/Walnut.png" class="ws-icon walnut-icon" alt="" style="width: 32px; height: 32px; margin-right: 8px;">';
+  iconMarkup = '<img src="src/assets/icons/Walnut.png" class="ws-icon walnut-icon" alt="" style="width: 40px; height: 40px; margin-right: 8px;">';
 } else if (f.key === 'Cardamom') {
   iconMarkup = '🌿';
 }
