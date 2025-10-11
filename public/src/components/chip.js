ed93b1a3d2af50bf3fe2bcd53d12b18a30827f43
AKAPP611 <Aknassar11@gmail.com>
2025-10-11 11:45:26 +0400

    Delete public/src/components/chip.js

--- public/src/components/chip.js
+++
@@ -1,6 +0,0 @@
-// chip.js - Chip component for displaying colored labels
-
-export function Chip({ text, color = '#999' }) {
-  const textColor = '#fff';
-  return `<span class="chip" style="background:${color};color:${textColor};">${text}</span>`;
-}
